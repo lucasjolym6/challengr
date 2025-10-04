73bfc1e22be6e77ab7f5ce95410a2afed880ba36
Raph13009 <raphaellevy027@gmail.com>
2025-10-04 10:28:36 +0100

    bug community fix

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -4,7 +4,12 @@ import CommunityFeed from "@/components/community/CommunityFeed";
 const Community = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#FFF7F2] via-white to-[#FFF2EC]">
-      <div className="pt-20 pb-6">
+      {/* Orange header section like Home page */}
+      <div className="relative bg-gradient-to-br from-orange-500/20 via-orange-400/15 to-orange-300/10 border-b border-border/40 -mt-20 pt-20">
+        <div className="h-16"></div>
+      </div>
+      
+      <div className="pb-6">
         <CommunityFeed />
       </div>
     </div>
